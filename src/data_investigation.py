d65907e3bfd380e6c0080c9959db4dc9d0c75e19
chili <ymf21971@gmail.com>
2025-08-04 12:29:38 +0300

    Add base dictionaries to each function in class DataAnalyzer

--- src/data_investigation.py
+++ src/data_investigation.py
@@ -19,19 +19,31 @@ class DataAnalyzer:
         print(f"shape {self.first_df.shape}")
 
     def total_tweets(self):
-        return
+        dict_total_tweets = {"total_tweets": {"antisemitic": 0,"non_antisemitic": 0,"total": 0,"unspecified" : 0}}
+
+        return dict_total_tweets
 
     def average_length(self):
-        return
+        dict_average_length = {"average_length": {"antisemitic": 0,"non_antisemitic": 0,"total": 0}}
+
+        return dict_average_length
+
+
 
     def common_words(self):
-        return
+        dict_common_words = {"common_words": {"total": []}}
+
+        return dict_common_words
 
     def longest_3_tweets(self):
-        return
+        dict_longest_3_tweets = {"longest_3_tweets": {"antisemitic": [],"non_antisemitic": []}}
+
+        return dict_longest_3_tweets
 
     def uppercase_words(self):
-        return
+        dict_uppercase_words = {"uppercase_words": {"antisemitic": 0,"non_antisemitic": 0,"total": 0}}
+
+        return dict_uppercase_words
 
 if __name__ == "__main__":
     d1 = DataAnalyzer()
